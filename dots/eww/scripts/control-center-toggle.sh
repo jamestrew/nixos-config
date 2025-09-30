96c187923fc29971839bed7c543b9dbcf71904f0
James Trew <j.trew10@gmail.com>
2025-09-30 00:38:58 -0400

    eww control center add power options and cal

--- dots/eww/scripts/control-center-toggle.sh
+++ dots/eww/scripts/control-center-toggle.sh
@@ -1,8 +1,4 @@
 #!/usr/bin/env bash
 
 # Toggle the control center window
-if eww active-windows | rg -q "control-center"; then
-  eww close control-center
-else
-  eww open control-center
-fi
+eww open --toggle control-center

cc3d04000f16691cc4e693ef4af3cfc190aee89e
James Trew <j.trew10@gmail.com>
2025-11-26 23:44:50 -0500

    update nix flake (v25.11)

--- nix/flake.nix
+++ nix/flake.nix
@@ -4,7 +4,7 @@
     nixpkgs.url = "nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "nixpkgs/nixos-24.11";
     home-manager = {
-      url = "github:nix-community/home-manager";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     sops-nix = {

96781f10e9c504cd846772cfd42dfe737733e5b3
James Trew <j.trew10@gmail.com>
2026-01-30 21:17:36 -0500

    update flake and switch to nix-ai-tools binary

--- nix/flake.nix
+++ nix/flake.nix
@@ -29,6 +29,10 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
+  nixConfig = {
+    extra-substituters = [ "https://cache.numtide.com" ];
+    extra-trusted-public-keys = [ "niks3.numtide.com-1:DTx8wZduET09hRmMtKdQDxNNthLQETkc/yaX7M4qK0g=" ];
+  };
   outputs =
     {
       self,
